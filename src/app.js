a4c5878c229ece6de81d044f6181360a188ac2e9
CheezItMan <chris@adadevelopersacademy.org>
2017-12-02 14:55:21 -0800

    Prior to installing Karma.

--- src/app.js
+++ src/app.js
@@ -1,12 +1,16 @@
 // Vendor Modules
 import $ from 'jquery';
 import _ from 'underscore';
-
-// CSS
-import './css/foundation.css';
+import 'foundation-sites/scss/foundation.scss';
 import './css/style.css';
 
-console.log('it loaded!');
+import Trip from './models/trip';
+
+
+const myTrip = new Trip({});
+
+// CSS
+
 
 $(document).ready( () => {
   $('main').html('<h1>Hello World!</h1>');
